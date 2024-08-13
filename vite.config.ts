46a9549e58693dad75ee729b0acc0ac140ee811a
AbulBashar38 <abulbasarofficial5403@gmail.com>
2024-08-14 04:20:25 +0600

    update docker file for env variable

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,15 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
+  plugins: [react()],
   preview: {
     port: 3000,
+    strictPort: true,
   },
-  plugins: [react()],
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:3000",
+  },
 });
